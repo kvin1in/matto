73f5ecf9512ee692a6d83d6fdb3b5e8dd0e75dc3
Kirill Sh <kshumov89@gmail.com>
2017-01-25 11:14:54 +0300

    Add animate

--- js/init.js
+++ js/init.js
@@ -7,5 +7,14 @@
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
 
+        var options = [
+            { selector: '#slider', offset: 100, callback: 'Materialize.fadeInImage("#slider"), 2000' },
+            { selector: '#carousel', offset: 100, callback: 'Materialize.fadeInImage("#carousel"), 2000' },
+            { selector: '#video', offset: 100, callback: 'Materialize.fadeInImage("#video"), 2000' },
+            { selector: '#news', offset: 100, callback: 'Materialize.fadeInImage("#news"), 2000' },
+        ];
+        Materialize.scrollFire(options);
+
+
     }); // end of document ready
 })(jQuery); // end of jQuery name space
